773da1e15c63ebc73a1b7fcb16ed4253489b8fd4
paradox-99 <rnayeem221523@bscse.uiu.ac.bd>
2025-12-09 00:14:32 +0600

    completed user account creation api

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,20 +14,27 @@ const getUsers = async (req, res) => {
 
 const createUser = async (req, res) => {
      const { email, phone, full_name, gender, date_of_birth, nid, profile_picture, street, city, district, postal_code, lat, lon } = req.body
+     console.log(postal_code);
+     
      const query = `INSERT INTO users (email, phone, full_name, gender, date_of_birth, nid, password_hash,  profile_picture, created_at) VALUES ($1, $2, $3, $4, $5, $6, 'sdskspassword', $7, now()) RETURNING id`
      try {
           const result = await pool.query(query, [email, phone, full_name, gender, date_of_birth, nid, profile_picture])
-          console.log(result.rows[0]);
-          if (result.rows.length !== 0) {
-               console.log(result.rows[0].id);
+          if (result.rows.length === 1) {
+               const userId = result.rows[0].id
                
+               const addressQuery = `INSERT INTO addresses (user_id, street, city, district, postal_code, lat, lon) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`
+               try {
+                    const finalResult = await pool.query(addressQuery, [userId, street, city, district, postal_code, lat, lon])
+
+                    if (finalResult.rowCount > 0)
+                         res.status(201).json({ message: 'User created successfully'})
+                    else
+                         res.status(500).json({ error: 'Failed to insert address' })
+               } catch (error) {
+                    res.status(500).json({ error: 'Error inserting address:' })
+               }
           }
-          // const userId = result.rows[0].id
-          // const addressQuery = `INSERT INTO addresses (user_id, street, city, district, postal_code, lat, lon) VALUES ($1, $2, $3, $4, $5, $6, $7)`
-          // await pool.query(addressQuery, [userId, street, city, district, postal_code, lat, lon])
-          res.status(201).json(result.rows[0])
      } catch (error) {
-          console.error('Error creating user:', error)
           res.status(500).json({ error: 'Internal Server Error' })
      }
 }
