e70bcffc8c7098c94d4291086c062f05f08b2601
jui-9545 <sumaiyajahan9545@gmail.com>
2025-12-15 12:55:42 +0600

    created function to update user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {getUsers, createUser} = require('../controllers/userController');
+const {getUsers, createUser, updateAddress, updateUser} = require('../controllers/userController');
 
 router.get('/users', getUsers);
 router.post('/createUser', createUser);
+router.put('/updateAddress', updateAddress);
+router.put('/updateUser', updateUser);
 
 module.exports = router;
