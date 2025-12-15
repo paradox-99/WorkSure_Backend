20734afd6e969692d9d2e0a5e49336f4ba8d9dc9
paradox-99 <rnayeem221523@bscse.uiu.ac.bd>
2025-12-15 12:15:47 +0600

    modified worker search function using prisma

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,4 @@
-// import pool from "../config/db.js"
-const { log } = require("console");
-const pool = require("../config/db");
 const prisma = require('../config/prisma');
-
 
 const getUsers = async (req, res) => {
      try {
@@ -10,9 +6,7 @@ const getUsers = async (req, res) => {
                where: {
                     role: 'client'
                }
-          })
-          // console.log(users);
-          
+          });
           res.status(200).json(users)
      } catch (error) {
           console.error('Error fetching users:', error)
@@ -22,28 +16,46 @@ const getUsers = async (req, res) => {
 
 const createUser = async (req, res) => {
      const { email, phone, full_name, gender, date_of_birth, nid, profile_picture, street, city, district, postal_code, lat, lon } = req.body
-     console.log(postal_code);
-     
-     const query = `INSERT INTO users (email, phone, full_name, gender, date_of_birth, nid, password_hash,  profile_picture, created_at) VALUES ($1, $2, $3, $4, $5, $6, 'sdskspassword', $7, now()) RETURNING id`
-     try {
-          const result = await pool.query(query, [email, phone, full_name, gender, date_of_birth, nid, profile_picture])
-          if (result.rows.length === 1) {
-               const userId = result.rows[0].id
-               
-               const addressQuery = `INSERT INTO addresses (user_id, street, city, district, postal_code, lat, lon) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`
-               try {
-                    const finalResult = await pool.query(addressQuery, [userId, street, city, district, postal_code, lat, lon])
 
-                    if (finalResult.rowCount > 0)
-                         res.status(201).json({ message: 'User created successfully'})
-                    else
-                         res.status(500).json({ error: 'Failed to insert address' })
-               } catch (error) {
-                    res.status(500).json({ error: 'Error inserting address:' })
-               }
-          }
+     try {
+          await prisma.$transaction(async (tx) => {
+               // 1️⃣ Create user
+               const user = await tx.users.create({
+                    data: {
+                         email,
+                         phone,
+                         full_name,
+                         gender,
+                         date_of_birth: new Date(date_of_birth),
+                         nid,
+                         password_hash: "sdskspassword",
+                         profile_picture,
+                         created_at: new Date()
+                    },
+                    select: {
+                         id: true
+                    }
+               });
+
+               // 2️⃣ Create address
+               await tx.addresses.create({
+                    data: {
+                         userId: user.id,
+                         street,
+                         city,
+                         district,
+                         postal_code,
+                         lat,
+                         lon
+                    }
+               });
+          });
+
+          res.status(201).json({ message: "User created successfully" });
+
      } catch (error) {
-          res.status(500).json({ error: 'Internal Server Error' })
+          console.error("Error creating user:", error);
+          res.status(500).json({ error: "Internal Server Error" });
      }
 }
 
--- controllers/workerController.js
+++ controllers/workerController.js
@@ -1,11 +1,13 @@
-const pool = require("../config/db");
-
+const prisma = require("../config/prisma");
 
 const getWorkers = async (req, res) => {
-     const query = 'SELECT * FROM users WHERE role=\'worker\''
      try {
-          const result = await pool.query(query)
-          res.status(200).json(result.rows)
+          const workers = await prisma.users.findMany({
+               where: {
+                    role: 'worker'
+               }
+          })
+          res.status(200).json(workers)
      } catch (error) {
           console.error('Error fetching users:', error)
           res.status(500).json({ error: 'Internal Server Error' })
@@ -15,9 +17,16 @@ const getWorkers = async (req, res) => {
 const createWorker = async (req, res) => {
      const { user_id, display_name, bio, years_experience, category_id, base_price, price_unit, skills, available_from, available_to, weekend, area_geometry } = req.body
 
-     const query = `INSERT INTO worker_profiles (user_id, display_name, bio, years_experience, created_at) VALUES ($1, $2, $3, $4, now()) RETURNING id`
      try {
-          const result = await pool.query(query, [user_id, display_name, bio, years_experience])
+          await prisma.worker_profiles.create({
+               data: {
+                    user_id,
+                    display_name,
+                    bio,
+                    years_experience,
+                    created_at: new Date()
+               }
+          })
           res.status(201).json({ message: 'Worker created successfully' })
      } catch (error) {
           res.status(500).json({ error: 'Internal Server Error' })
@@ -27,9 +36,16 @@ const createWorker = async (req, res) => {
 const createWorkerService = async (req, res) => {
      const { user_id, category_id, base_price, price_unit, skills } = req.body
 
-     const query = `INSERT INTO worker_services (worker_id, category_id, base_price, price_unit, skills) VALUES ($1, $2, $3, $4, $5) RETURNING id`
      try {
-          const result = await pool.query(query, [user_id, category_id, base_price, price_unit, skills])
+          await prisma.worker_services.create({
+               data: {
+                    user_id,
+                    category_id,
+                    base_price,
+                    price_unit,
+                    skills
+               }
+          })
           res.status(201).json({ message: 'Worker Service created successfully' })
      } catch (error) {
           res.status(500).json({ error: 'Internal Server Error' })
@@ -37,11 +53,17 @@ const createWorkerService = async (req, res) => {
 }
 
 const createWorkerAvailability = async (req, res) => {
-     const { user_id, available_from, available_to, weekend, area_geometry } = req.body
+     const { user_id, available_from, available_to, weekend } = req.body
 
-     const query = `INSERT INTO worker_availabilities (worker_id, available_from, available_to, weekend, area_geometry) VALUES ($1, $2, $3, $4) RETURNING id`
      try {
-          const result = await pool.query(query, [user_id, available_from, available_to, weekend])
+          await prisma.availabilities.create({
+               data: {
+                    worker_id: user_id,
+                    available_from: new Date(available_from),
+                    available_to: new Date(available_to),
+                    weekend
+               }
+          })
           res.status(201).json({ message: 'Worker Availability created successfully' })
      } catch (error) {
           res.status(500).json({ error: 'Internal Server Error' })
@@ -51,42 +73,41 @@ const createWorkerAvailability = async (req, res) => {
 const searchWorkers = async (req, res) => {
      const { categorySlug, lat, lon, radiusMeters } = req.query
 
-     const query = `
+     console.log(categorySlug);
+     
+
+     try {
+          const workers = await prisma.$queryRaw`
           WITH cat AS (
-               SELECT id FROM service_categories WHERE slug = $1
+            SELECT id FROM service_categories WHERE slug = ${categorySlug}
           )
           SELECT
-          u.id AS user_id,
-          wp.display_name,
-          wp.avg_rating,
-          a.lat,
-          a.lon,
-          ws.base_price,
-          ST_Distance(
-               ST_SetSRID(ST_MakePoint(a.lon, a.lat), 4326)::geography,
-               ST_SetSRID(ST_MakePoint($3, $2), 4326)::geography
-          ) AS distance_m
+            u.id AS user_id,
+            wp.display_name,
+            wp.avg_rating,
+            a.lat,
+            a.lon,
+            ws.base_price,
+            ST_Distance(
+              ST_SetSRID(ST_MakePoint(a.lon, a.lat), 4326)::geography,
+              ST_SetSRID(ST_MakePoint(${lon}, ${lat}), 4326)::geography
+            ) AS distance_m
           FROM worker_services ws
           JOIN users u ON ws.user_id = u.id
           JOIN worker_profiles wp ON wp.user_id = u.id
           LEFT JOIN addresses a ON a.user_id = u.id
           WHERE ws.category_id = (SELECT id FROM cat)
-          AND wp.verification = 'verified'
-          AND a.lat IS NOT NULL
-          AND a.lon IS NOT NULL
-          AND ST_DWithin(
-               ST_SetSRID(ST_MakePoint(a.lon, a.lat), 4326)::geography,
-               ST_SetSRID(ST_MakePoint($3, $2), 4326)::geography,
-               $4
-          )
-          ORDER BY wp.avg_rating DESC NULLS LAST, distance_m ASC
-          LIMIT $5;`;
-
-     const params = [categorySlug, lat, lon, radiusMeters];
-
-     try {
-          const result = await pool.query(query, params)
-          res.status(200).json(result.rows)
+            AND wp.verification = 'verified'
+            AND a.lat IS NOT NULL
+            AND a.lon IS NOT NULL
+            AND ST_DWithin(
+              ST_SetSRID(ST_MakePoint(a.lon, a.lat), 4326)::geography,
+              ST_SetSRID(ST_MakePoint(${lon}, ${lat}), 4326)::geography,
+              ${radiusMeters}
+            )`;
+            console.log(workers);
+            
+          res.status(200).json(workers)
      } catch (error) {
           console.error('Error searching workers:', error)
           res.status(500).json({ error: 'Internal Server Error' })
