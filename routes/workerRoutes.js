fa743b0167f4d0d4648dc7f1466aad0e16ad2879
tomalalalala <mtomal222149@bscse.uiu.ac.bd>
2025-12-15 13:52:37 +0600

    created availability table update function

--- routes/workerRoutes.js
+++ routes/workerRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getWorkers, createWorker, createWorkerAvailability, createWorkerService, searchWorkers, updateWorkerProfile,updateWorkerService} = require('../controllers/workerController');
+const {getWorkers, createWorker, createWorkerAvailability, createWorkerService, searchWorkers, updateWorkerProfile,updateWorkerService, updateAvailability} = require('../controllers/workerController');
 
 router.get('/workers', getWorkers);
 router.post('/workers', createWorker);
@@ -9,5 +9,6 @@ router.post('/workers/availability', createWorkerAvailability);
 router.get('/workers/search', searchWorkers);
 router.patch('/updateWorkerProfile', updateWorkerProfile);
 router.patch('/updateWorkerService', updateWorkerService);
+router.patch('/updateAvailability/:id', updateAvailability);
 
 module.exports = router;
