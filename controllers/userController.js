c1c97247e042d9b83b422f6336ba82a7ba7d1b9c
paradox-99 <rnayeem221523@bscse.uiu.ac.bd>
2026-01-04 13:08:51 +0600

    added route to get user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,6 @@ const createUser = async (req, res) => {
 
      try {
           await prisma.$transaction(async (tx) => {
-               // 1️⃣ Create user
                const user = await tx.users.create({
                     data: {
                          email,
@@ -142,4 +141,30 @@ const updateUser = async (req, res) => {
      }
 };
 
-module.exports = { getUsers, createUser, updateAddress, updateUser };
+const getUserData = async (req, res) => {
+     const { email } = req.params;
+
+     try {
+          const user = await prisma.users.findUnique({
+               where: {
+                    email,
+                    role: 'client'
+               },
+               include: {
+                    addresses: true
+               }
+          });
+          
+
+          if (!user) {
+               return res.status(404).json({ error: "User not found" });
+          }
+
+          res.status(200).json(user);
+     } catch (error) {
+          console.error("Error fetching user data:", error);
+          res.status(500).json({ error: "Failed to fetch user data" });
+     }
+};
+
+module.exports = { getUsers, createUser, updateAddress, updateUser, getUserData };
