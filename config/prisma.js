b4c019f282da4dd8c36396e80712b5d48b2619aa
paradox-99 <rnayeem221523@bscse.uiu.ac.bd>
2026-01-26 11:23:46 +0600

    Fixed maximum connection pooling error

--- config/prisma.js
+++ config/prisma.js
@@ -10,11 +10,11 @@ if (!globalForPrisma.prisma) {
   // 1. Create a Postgres connection pool with proper limits
   const pool = new Pool({ 
     connectionString: process.env.WORKSURE_DATABASE_URL,
-    max: 20,              // Maximum number of connections in the pool (increased from 10)
-    min: 2,               // Minimum number of connections to keep alive
-    idleTimeoutMillis: 30000,  // Close idle connections after 30 seconds (reduced from 60)
-    connectionTimeoutMillis: 20000,  // Return an error after 10 seconds if connection could not be established
-    allowExitOnIdle: true  // Allow the pool to exit when all connections are idle
+    max: 1,              // Maximum number of connections in the pool (increased from 10)
+    // min: 2,               // Minimum number of connections to keep alive
+    // idleTimeoutMillis: 60000,  // Close idle connections after 60 seconds (increased from 30)
+    // connectionTimeoutMillis: 20000,  // Return an error after 20 seconds if connection could not be established
+    // allowExitOnIdle: true  // Allow the pool to exit when all connections are idle
   });
 
   // 2. Create the Prisma Adapter
@@ -22,33 +22,33 @@ if (!globalForPrisma.prisma) {
 
   // 3. Pass the adapter to PrismaClient
   globalForPrisma.prisma = new PrismaClient({ adapter });
-  globalForPrisma.pool = pool;
+  // globalForPrisma.pool = pool;
 }
 
 const prisma = globalForPrisma.prisma;
 
 // Handle graceful shutdown
-process.on('beforeExit', async () => {
-  await prisma.$disconnect();
-  if (globalForPrisma.pool) {
-    await globalForPrisma.pool.end();
-  }
-});
+// process.on('beforeExit', async () => {
+//   await prisma.$disconnect();
+//   if (globalForPrisma.pool) {
+//     await globalForPrisma.pool.end();
+//   }
+// });
 
-process.on('SIGINT', async () => {
-  await prisma.$disconnect();
-  if (globalForPrisma.pool) {
-    await globalForPrisma.pool.end();
-  }
-  process.exit(0);
-});
+// process.on('SIGINT', async () => {
+//   await prisma.$disconnect();
+//   if (globalForPrisma.pool) {
+//     await globalForPrisma.pool.end();
+//   }
+//   process.exit(0);
+// });
 
-process.on('SIGTERM', async () => {
-  await prisma.$disconnect();
-  if (globalForPrisma.pool) {
-    await globalForPrisma.pool.end();
-  }
-  process.exit(0);
-});
+// process.on('SIGTERM', async () => {
+//   await prisma.$disconnect();
+//   if (globalForPrisma.pool) {
+//     await globalForPrisma.pool.end();
+//   }
+//   process.exit(0);
+// });
 
 module.exports = prisma;
